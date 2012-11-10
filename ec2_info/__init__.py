7a50a055abe2d7af50cbfcb04c06b616c5df88f3
Robert Cotey <coteyr@coteyr.net>
2012-11-09 20:23:29 -0500

    add some comments

--- ec2_info/__init__.py
+++ ec2_info/__init__.py
@@ -24,36 +24,36 @@ class Ec2InfoLens(SingleScopeLens):
         search_hint = 'Search EC2 Info'
         icon = 'ec2-info.svg'
         search_on_blank = True
-
-    # TODO: Add your categories
-    #example_category = ListViewCategory("Examples", 'help')
+    # I tried to use standard icons that made sense
     running_hosts_category = ListViewCategory("Running Hosts", 'network-server')
     stopped_hosts_category = ListViewCategory("Stopped Hosts", 'dialog-error')
-    home = expanduser("~")
-    api_list = []
+    home = expanduser("~")  # perhaps not the best, but seems like it would work to get the home directory.
+    api_list = []  # for storing api get results to not log the snot out of unity.
     last_update = datetime.now() - timedelta(days=7)  # the first time is out of date so lets force it.
 
     def search(self, search, results):
         if self.last_update < datetime.now() - timedelta(minutes=5):  # update evey 5 mins
             self.last_update = datetime.now()  # cheap caching
-            del self.api_list[0:len(self.api_list)]
+            del self.api_list[0:len(self.api_list)]  # clear refrences too, because I don't know what's happening behind the curtian
             for match in glob.glob("%s/.ec2-info/*.conf" % self.home):
                 print("Getting New Data from AWS")
                 p = subprocess.Popen(["aws", "din", "--yaml", "--secrets-file=%s" % match], shell=False, stdout=subprocess.PIPE)
-                result = p.stdout.read()
-                name = match[self.home.__len__() + 11:-5]
+                result = p.stdout.read()  # decided on this as the best method, but there is always communicate
+                name = match[self.home.__len__() + 11:-5]  # name derived from config file
                 self.api_list.append((name, result))
 
         for item in self.api_list:
-            #
             name = item[0]
             result = yaml.load(item[1])
             for reservation in result['reservationSet']:
-                #print(reservation['instancesSet'])
                 for instance in reservation['instancesSet']:
-                    print(instance)
                     instanceID = instance["instanceId"]
                     instanceState = instance["instanceState"]["name"]
+                    # These horrid try blocks are the best I could come up with. I could DRY it a bit, but honestly that
+                    # seems like more problem then just doing this. It's important to note that were recursing the stored
+                    # cached data instead of getting new data. This helps with quickly searching.
+                    # Though the cache has to be read every time a key is pressed. I can't find a way around that
+                    # using singlet
                     try:
                         instanceIP = instance["ipAddress"]
                     except:
@@ -94,11 +94,12 @@ class Ec2InfoLens(SingleScopeLens):
         pass
 
     def handle_uri(self, scope, uri):
+        # not a real URI but it works
         ip = uri.partition(':')[2]
         platform = uri.partition(':')[0]
         print(platform)
         print(ip)
-        if uri != "No IP":
+        if ip != "No IP":  # skip the offline hosts
             if platform != "windows":
                 subprocess.Popen(["gnome-terminal", "--window", "-e", "ssh %s" % uri])
             else:
